749f5e51b3ba2e815964e199bd3db1764d5c29fe
GPrakasha <gprakasha234@gmail.com>
2021-06-21 01:05:57 +0530

    fixing a few bugs

--- src/containers/AddNote.js
+++ src/containers/AddNote.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import {  Form, Button } from 'react-bootstrap';
 import { Modal, useModal } from 'react-morphing-modal';
 import 'react-morphing-modal/dist/ReactMorphingModal.css';
 import firebase from 'firebase';
 import { useHistory } from 'react-router-dom';
+import { ThemeContext } from '../theme/ThemeContext';
+import CustomLoader from '../common/Loader';
 
 function AddNote(props) {
 
@@ -35,20 +37,24 @@ function AddNote(props) {
 
     useEffect(() => {
         document.getElementById("add").click();
-    }, [])
+    }, []);
+
+    const theme = useContext(ThemeContext);
 
     return (
         <div className="vh-100 d-flex w-100">
-            <div id="add" {...getTriggerProps({ background: style.background, color: style.color })}>Loading</div>
+            <div id="add" {...getTriggerProps({ background: theme.backgroundColor, color: theme.color })} className="m-auto">
+                <CustomLoader/>
+            </div>
             {
                 <Modal {...modalProps}>
                     <Form>
                         <Form.Group>
-                            <Form.Control  style={{ background: style.background, color: style.color }} required type="text" onChange={(e) => setTitle(e.target.value)} />
+                            <Form.Control  style={{ background: theme.backgroundColor, color: theme.color }} required type="text" onChange={(e) => setTitle(e.target.value)} />
                         </Form.Group>
                         <hr></hr>
                         <Form.Group>
-                            <Form.Control style={{ height: "70vh", background: style.background, color: style.color }} as="textarea" rows={3} onChange={(e) => setNote(e.target.value)} />
+                            <Form.Control style={{ height: "70vh", background: theme.backgroundColor, color: theme.color }} as="textarea" rows={3} onChange={(e) => setNote(e.target.value)} />
                         </Form.Group>
                         <Form.Group className="d-flex">
                             <Button onClick={() => history.push('/')} variant={(localStorage.currentTheme === "light") ? "outline-secondary" : "primary"}>
